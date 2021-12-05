e386c272855bec368f977eb315fce0e70deeee24
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2021-12-05 02:59:53 +0100

    optimized it a little

--- 2021/day1/part1.py
+++ 2021/day1/part1.py
@@ -2,9 +2,4 @@ with open("input.txt", "r") as f:
     nums = f.readlines()
     nums = [int(e) for e in nums]
 
-    increased = 0
-    for i in range(1, len(nums)):
-        if nums[i] > nums[i-1]:
-            increased += 1
-
-    print(increased)
+    print(sum(1 for i in range(1, len(nums)) if nums[i] > nums[i-1]))
